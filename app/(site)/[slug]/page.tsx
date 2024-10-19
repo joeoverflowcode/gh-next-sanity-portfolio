17fa36e34687ec02d89714dec82678d49a2fd612
Joe Aguado <aguado.joe@gmail.com>
2024-10-19 12:17:30 -0500

    deployment changes

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,19 +1,33 @@
-import { getPage } from "@/sanity/sanity-utils";
-import { PortableText } from "@portabletext/react"
+import { getPage, getPages } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
 
 type Props = {
-    params: {slug: string}
+  params: { slug: string };
+};
+
+export default async function Page({ params }: Props) {
+  const page = await getPage(params.slug);
+
+  return (
+    <div>
+      <h1>{page.title}</h1>
+
+      <div>
+        <PortableText value={page.content} />
+      </div>
+    </div>
+  );
 }
 
-export default async function Page({params}: Props) {
-    const page = await getPage(params.slug)
+// Generate static params for dynamic routes
+export async function generateStaticParams() {
+  // Fetch all page slugs from Sanity
+  const pages = await getPages(); // Adjust this to match your sanity-utils function
+  const paths = pages.map((page: { slug: string }) => ({
+    slug: page.slug,
+  }));
 
-    return (
-        <div>
-            <h1>{page.title}</h1>
-
-            <div><PortableText value={page.content} /></div>
-        </div>
-    )
-
+  return paths.map((path) => ({
+    params: path,
+  }));
 }
