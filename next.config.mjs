da7e2fcbab70f84d77e3c8f3f1408a15fa623342
Joe Aguado <aguado.joe@gmail.com>
2024-10-19 11:55:20 -0500

    deployment changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'cdn.sanity.io',
-            port: '',
-          },
-        ],
-      },
-};
-
-export default nextConfig;
-
-
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
-//   output: "export",
-//   basePath: "/nextjs-github-pages",
-//   images: {
-//     unoptimized: true,
-//   },
+
+//     images: {
+//         remotePatterns: [
+//           {
+//             protocol: 'https',
+//             hostname: 'cdn.sanity.io',
+//             port: '',
+//           },
+//         ],
+//       },
 // };
 
 // export default nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
