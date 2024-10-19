f90525c6f0cb3197309f39e82674943bb007db56
Joe Aguado <aguado.joe@gmail.com>
2024-10-19 12:09:07 -0500

    deployment changes

--- app/(site)/projects/[project]/page.tsx
+++ app/(site)/projects/[project]/page.tsx
@@ -1,30 +1,51 @@
-import { getProject } from '@/sanity/sanity-utils'
+import { getProject, getProjects } from '@/sanity/sanity-utils';
 import { PortableText } from '@portabletext/react';
 import Image from 'next/image';
-import React from 'react'
+import React from 'react';
 import { Button } from '@/components/ui/button';
 
 type Props = {
-    params : {
-        project:string
-    }
-}
+  params: {
+    project: string;
+  };
+};
 
-const Project = async ({params}: Props) => {
-  
-    const slug = params.project
-    const project = await getProject(slug)
-  
-    return (
-    <div className='flex flex-col items-center'>
-        <header className='my-4'>
-          <h1 className='font-bold text-2xl'>{project.name}</h1>
-        </header>
-    <Image src={project.image} width={500} height={500} alt='image' className='my-4'/>
-    <PortableText value={project.content}/>
-          <Button className='mt-2'><a href={project.url} title='view project' target='_blank' rel='noopener noreferrer' className=''>View Project</a></Button>  
+const Project = async ({ params }: Props) => {
+  const slug = params.project;
+  const project = await getProject(slug);
+
+  return (
+    <div className="flex flex-col items-center">
+      <header className="my-4">
+        <h1 className="font-bold text-2xl">{project.name}</h1>
+      </header>
+      <Image src={project.image} width={500} height={500} alt="image" className="my-4" />
+      <PortableText value={project.content} />
+      <Button className="mt-2">
+        <a
+          href={project.url}
+          title="view project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          View Project
+        </a>
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
+
+// Generate static params for dynamic routes
+export async function generateStaticParams() {
+  // Fetch all project slugs from Sanity
+  const projects = await getProjects(); // Adjust this to match your sanity-utils function
+  const paths = projects.map((project: { slug: string }) => ({
+    project: project.slug,
+  }));
+
+  return paths.map((path) => ({
+    params: path,
+  }));
+}
